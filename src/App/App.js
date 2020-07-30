51aca862f5d0b227941c29fd37153993874779be
carflor <cfdeclet@gmail.com>
2020-07-29 19:09:49 -0600

    Add components folders and files

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Route, Switch, Link } from 'react-router-dom'
+import Holidays from '../Holidays/Holidays'
+import Saved from '../Saved/Saved'
+
+const main = (
+  <main className="app">
+    <nav className="nav">
+      <p className="page-title">EasyTravel</p>
+      {/* saved icon  */}
+    </nav>
+    <section className="search-box">
+      <input type="search-bar">
+
+      </input>
+      {/* ICON FOR SEARCH BAR */}
+    </section>
+  </main> 
+)
 
 function App() {
+
   return (
-    <main className="app">
-      <nav className="nav">EasyTravel</nav>
-    </main>
-  );
+    <Switch>
+      <Route path="/holidays/:country" render={() => <Holidays />} />
+      <Route path="/saved" render={() => <Saved />} />
+      <Route path="/" render={() => main} />
+    </Switch>
+  )
 }
 
 export default App;
---
+++ src/Holidays/Holidays.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Holidays() {
+
+  return (
+    <h1>Holidays</h1>
+  )
+}
+
+export default Holidays
