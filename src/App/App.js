8de11a0d4f894dac6387f38ddd61415d70109ff9
carflor <cfdeclet@gmail.com>
2020-07-29 18:03:51 -0600

    Initial commit, erased unsued files/docs, add PR templated and basic setup

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <nav className="nav">EasyTravel</nav>
+    </main>
+  );
+}
+
+export default App;
